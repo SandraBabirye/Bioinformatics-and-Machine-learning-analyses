75ff74f081285cae1cc2fa8fcb7e26546de1b42a
Sandra Babirye <99010085+SandraBabirye@users.noreply.github.com>
2023-10-26 07:59:31 +0300

    Update annotation.sh

--- Bioinformatics scripts/annotation.sh
+++ Bioinformatics scripts/annotation.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-
+# Create a directory to store the output from Annotation
 mkdir Annotation
 
-for infile in filtered_contigs_output_500/*                       
+for infile in Results/assembly_output/*
 do
-   # Extract the sample name from the infile (removing leading path and file extension)
-   sample_name=$(basename "$infile" _filtered_contigs.fasta)
+   # Extract the sample name from the infile (removing leading path)
+   sample_name="$(basename "$infile")"
    
-
+   # Remove the "/contig.fasta" part from the sample name
+   sample_name="${sample_name%/contigs.fasta}"
+   
    prokka  --cpus 4 --kingdom Bacteria --usegenus --genus Mycobacterium  --species tuberculosis --prefix "${sample_name}" --locustag "${sample_name}"  ${infile} \
    --outdir "Annotation/${sample_name}"
 
